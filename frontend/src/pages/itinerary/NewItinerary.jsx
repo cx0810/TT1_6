ee718adaea713e5b787439423f76493f6a19cbd9
Lin Peishan <linpeishann@gmail.com>
2024-01-20 13:06:56 +0800

    added breadcrumb

--- frontend/src/pages/itinerary/NewItinerary.jsx
+++ frontend/src/pages/itinerary/NewItinerary.jsx
@@ -14,6 +14,29 @@ import {
     Select,
     InputLabel,
   } from '@mui/material';
+import { emphasize, styled } from '@mui/material/styles';
+import Chip from '@mui/material/Chip';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+
+const StyledBreadcrumb = styled(Chip)(({ theme }) => {
+    const backgroundColor =
+      theme.palette.mode === 'light'
+        ? theme.palette.grey[100]
+        : theme.palette.grey[800];
+    return {
+      backgroundColor,
+      height: theme.spacing(3),
+      color: theme.palette.text.primary,
+      fontWeight: theme.typography.fontWeightRegular,
+      '&:hover, &:focus': {
+        backgroundColor: emphasize(backgroundColor, 0.06),
+      },
+      '&:active': {
+        boxShadow: theme.shadows[1],
+        backgroundColor: emphasize(backgroundColor, 0.12),
+      },
+    };
+  }); // TypeScript only: need a type cast here because https://github.com/Microsoft/TypeScript/issues/26591  
 
 const NewItinerary = () => {
 
@@ -24,9 +47,17 @@ const NewItinerary = () => {
   const handleSubmit = () => {
     return; }
   return (
-    <>
+    <Box margin={4}>
+    <Breadcrumbs aria-label="breadcrumb">
+        <StyledBreadcrumb
+        component="a"
+        href="#"
+        label="Dashboard"
+        />
+        <StyledBreadcrumb component="a" href="#" label="New Itinerary" />
+    </Breadcrumbs>
       <section>
-        <h1>New Destination</h1>
+        <h1>New Itinerary</h1>
         <FormControl onSubmit={handleSubmit}>
           <label htmlFor="name">Destination Name:</label>
           <input
@@ -53,7 +84,7 @@ const NewItinerary = () => {
           <Button>Create Itinerary</Button>
         </FormControl>
       </section>
-    </>
+    </Box>
   )
 }
 export default NewItinerary
