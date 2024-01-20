e85743b7ee89b4dc59a5badd858867e697b30ee0
Tan Jia Jia <71870881+jiajiatan@users.noreply.github.com>
2024-01-20 14:10:01 +0800

    align center createdestination and editdestination

--- frontend/src/pages/itinerary/editdestination.jsx
+++ frontend/src/pages/itinerary/editdestination.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import { FormControl } from "@mui/material";
 import { Button } from "@mui/material";
-import '../../assets/Form.css';
+import "../../assets/Form.css";
+import Grid from "@mui/material/Grid";
 
 const Destination = () => {
-
-  const [name, setname] = useState('');
-  const [cost, setcost] = useState('');
-  const [notes, setnotes] = useState('');
+  const [name, setname] = useState("");
+  const [cost, setcost] = useState("");
+  const [notes, setnotes] = useState("");
 
   const handleSubmit = () => {
     return;
@@ -16,40 +16,73 @@ const Destination = () => {
   return (
     <>
       <section>
-        <h1>Edit Destination</h1>
-        <FormControl onSubmit={handleSubmit} style={{ margin: "20px auto", textAlign: "center" }}>
-          <label htmlFor="name">Destination Name:</label>
-          <input
-            type="text"
-            id="name"
-            onChange={(e) => setname(e.target.value)}
-            value={name}
-            required
-            style={{ width: "100%", height: "40px", margin: "5px auto", padding: "3px 7px", fontSize: "17px", textAlign: "center" }}
-          />
-          <label htmlFor="cost">Cost</label>
-          <input
-            type="text"
-            id="cost"
-            onChange={(e) => setcost(e.target.value)}
-            value={cost}
-            required
-            style={{ width: "100%", height: "40px", margin: "5px auto", padding: "3px 7px", fontSize: "17px", textAlign: "center" }}
-          />
-          <label htmlFor="notes">Notes</label>
-          <input
-            type="text"
-            id="notes"
-            autoComplete="off"
-            onChange={(e) => setnotes(e.target.value)}
-            value={notes}
-            required
-            style={{ width: "100%", height: "40px", margin: "5px auto", padding: "3px 7px", fontSize: "17px", textAlign: "center" }}
-          />
-          <Button type="submit" variant="contained" className="btn btn-block">
-            Edit Destination
-          </Button>
-        </FormControl>
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justify="center"
+          style={{ minHeight: "100vh" }}
+        >
+          <h1>Edit Destination</h1>
+          <FormControl
+            onSubmit={handleSubmit}
+            style={{ margin: "20px auto", textAlign: "center" }}
+          >
+            <label htmlFor="name">Destination Name:</label>
+            <input
+              type="text"
+              id="name"
+              onChange={(e) => setname(e.target.value)}
+              value={name}
+              required
+              style={{
+                width: "100%",
+                height: "40px",
+                margin: "5px auto",
+                padding: "3px 7px",
+                fontSize: "17px",
+                textAlign: "center",
+              }}
+            />
+            <label htmlFor="cost">Cost</label>
+            <input
+              type="text"
+              id="cost"
+              onChange={(e) => setcost(e.target.value)}
+              value={cost}
+              required
+              style={{
+                width: "100%",
+                height: "40px",
+                margin: "5px auto",
+                padding: "3px 7px",
+                fontSize: "17px",
+                textAlign: "center",
+              }}
+            />
+            <label htmlFor="notes">Notes</label>
+            <input
+              type="text"
+              id="notes"
+              autoComplete="off"
+              onChange={(e) => setnotes(e.target.value)}
+              value={notes}
+              required
+              style={{
+                width: "100%",
+                height: "40px",
+                margin: "5px auto",
+                padding: "3px 7px",
+                fontSize: "17px",
+                textAlign: "center",
+              }}
+            />
+            <Button type="submit" variant="contained" className="btn btn-block">
+              Edit Destination
+            </Button>
+          </FormControl>
+        </Grid>
       </section>
     </>
   );
--- frontend/src/pages/itinerary/newdestination.jsx
+++ frontend/src/pages/itinerary/newdestination.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import { Button } from "@mui/material";
 import { FormControl } from "@mui/material";
+import Grid from "@mui/material/Grid";
 
-import '../../assets/Form.css';
+import "../../assets/Form.css";
 
 const Destination = () => {
-
-  const [name, setname] = useState('');
-  const [cost, setcost] = useState('');
-  const [notes, setnotes] = useState('');
+  const [name, setname] = useState("");
+  const [cost, setcost] = useState("");
+  const [notes, setnotes] = useState("");
 
   const handleSubmit = () => {
     return;
@@ -17,40 +17,73 @@ const Destination = () => {
   return (
     <>
       <div classname="formSectionCSS">
-        <h1>New Destination</h1>
-        <FormControl onSubmit={handleSubmit} style={{ margin: "20px auto", textAlign: "center" }}>
-          <label htmlFor="name">Destination Name:</label>
-          <input
-            type="text"
-            id="name"
-            onChange={(e) => setname(e.target.value)}
-            value={name}
-            required
-            style={{ width: "100%", height: "40px", margin: "5px auto", padding: "3px 7px", fontSize: "17px", textAlign: "center" }}
-          />
-          <label htmlFor="cost">Cost</label>
-          <input
-            type="text"
-            id="cost"
-            onChange={(e) => setcost(e.target.value)}
-            value={cost}
-            required
-            style={{ width: "100%", height: "40px", margin: "5px auto", padding: "3px 7px", fontSize: "17px", textAlign: "center" }}
-          />
-          <label htmlFor="notes">Notes</label>
-          <input
-            type="text"
-            id="notes"
-            autoComplete="off"
-            onChange={(e) => setnotes(e.target.value)}
-            value={notes}
-            required
-            style={{ width: "100%", height: "40px", margin: "5px auto", padding: "3px 7px", fontSize: "17px", textAlign: "center" }}
-          />
-          <Button type="submit" variant="contained" className="btn btn-block">
-            New Destination
-          </Button>
-        </FormControl>
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justify="center"
+          style={{ minHeight: "100vh" }}
+        >
+          <h1>New Destination</h1>
+          <FormControl
+            onSubmit={handleSubmit}
+            style={{ margin: "20px auto", textAlign: "center" }}
+          >
+            <label htmlFor="name">Destination Name:</label>
+            <input
+              type="text"
+              id="name"
+              onChange={(e) => setname(e.target.value)}
+              value={name}
+              required
+              style={{
+                width: "100%",
+                height: "40px",
+                margin: "5px auto",
+                padding: "3px 7px",
+                fontSize: "17px",
+                textAlign: "center",
+              }}
+            />
+            <label htmlFor="cost">Cost</label>
+            <input
+              type="text"
+              id="cost"
+              onChange={(e) => setcost(e.target.value)}
+              value={cost}
+              required
+              style={{
+                width: "100%",
+                height: "40px",
+                margin: "5px auto",
+                padding: "3px 7px",
+                fontSize: "17px",
+                textAlign: "center",
+              }}
+            />
+            <label htmlFor="notes">Notes</label>
+            <input
+              type="text"
+              id="notes"
+              autoComplete="off"
+              onChange={(e) => setnotes(e.target.value)}
+              value={notes}
+              required
+              style={{
+                width: "100%",
+                height: "40px",
+                margin: "5px auto",
+                padding: "3px 7px",
+                fontSize: "17px",
+                textAlign: "center",
+              }}
+            />
+            <Button type="submit" variant="contained" className="btn btn-block">
+              New Destination
+            </Button>
+          </FormControl>
+        </Grid>
       </div>
     </>
   );
