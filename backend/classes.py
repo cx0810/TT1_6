b38ae1d5797c9b614dee342f0455902ef6967e7c
Lim-Huiee <huiee61@gmail.com>
2024-01-20 10:53:48 +0800

    added some backend features

--- backend/classes.py
+++ backend/classes.py
@@ -7,8 +7,7 @@ app = Flask(__name__)
 
 db = SQLAlchemy(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/cpaos'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/techtrek24'
 
 class User(db.Model):
     def __init__(self, id, first_name, last_name, password, username):
@@ -27,7 +26,7 @@ class User(db.Model):
     username = db.Column(db.String(20))
 
 
-class Itinerary:
+class Itinerary(db.Model):
     def __init__(self, id, country_id, user_id, budget, title):
         self.id = id 
         self.country_id = country_id
@@ -46,7 +45,7 @@ class Itinerary:
     # ClinicianSchoolAppointments = db.relationship('ClinicianSchoolAppointment', backref='faculty', lazy=True, passive_deletes=True)
 
 
-class ItineraryDestination:
+class ItineraryDestination(db.Model):
     def __init__(self, id, itinerary_id, destination_id):
         self.id = id 
         self.itinerary_id = itinerary_id
@@ -57,7 +56,7 @@ class ItineraryDestination:
     destination_id = db.Column(db.Integer, db.ForeignKey('Destination.id'), nullable=False)
     
 
-class Country:
+class Country(db.Model):
     def __init__(self, id, name):
         self.id = id 
         self.name = name
@@ -68,7 +67,7 @@ class Country:
     
     name = db.Column(db.String(50))
 
-class Destination:
+class Destination(db.Model):
     def __init__(self, id, country_id, cost, name, notes):
         self.id = id 
         self.country_id = country_id
---
+++ backend/features/user.py
@@ -0,0 +1,97 @@
+from flask import Blueprint, jsonify, request
+from classes import db, User
+
+user_bp = Blueprint('user_bp', __name__)
+
+@user_bp.route('/get_user/<user_id>', methods=['GET'])
+def get_user(user_id):
+    try:
+        user = User.query.filter_by(id=user_id).first()
+
+        user_obj = {
+            'id': user.id,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'password': user.password,
+            'username': user.username
+        }
+
+        return jsonify({"code": 200, "data": user_obj}), 200
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+@user_bp.route('/create_user', methods=['POST'])
+def create_user():
+    #create user
+    try:
+        data = request.get_json()
+
+        if not data or 'id' not in data: 
+            return jsonify({"code": 400, "message": "Invalid data"}), 400
+        
+        #check if user has already existed
+        existing_user = User.query.filter_by(id=data['id']).first()
+        if existing_user:
+            return jsonify({"code": 409, "message": "User already exist"}), 409
+        
+        new_user = User(
+            id = data['id'],
+            first_name = data['first_name'],
+            last_name = data['last_name'],
+            password = data['password'],
+            username = data['username']
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return jsonify({"code": 201, "username": data['id']}), 201
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@user_bp.route('/update_user', methods=['PUT'])
+def update_user():
+    #edit user
+    try:
+        data = request.get_json()
+        user_id = data.get('id')
+
+        if not data or not user_id:
+            return jsonify({"code": 400, "message": "Invalid data"})
+        
+        existing_user = User.query.filter_by(id=data['id']).first()
+        if not existing_user:
+            return jsonify({ "code": 404, "message": "User not found"}), 404
+
+        setattr(existing_user, 'first_name', data['first_name'])
+        setattr(existing_user, 'last_name', data['last_name'])
+        setattr(existing_user, 'password', data['password'])
+        setattr(existing_user, 'username', data['username'])
+
+        db.session.commit()
+        return jsonify({"code": 200, "message": f"User details updated successfully"}), 200
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@user_bp.route('/delete_user', methods=['DELETE'])
+def delete_user():
+    #delete user
+    try:
+        data = request.get_json()
+
+        if not data or 'id' not in data: 
+            return jsonify({"code": 400, "message": "Invalid data"}), 400
+        
+        user = User.query.filter_by(id=data['id']).first()
+
+        db.session.delete(user.id)
+        db.session.commit()
+
+        return jsonify({"code": 200, "message": f"User ID: {user.id} was deleted successfully"}), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
