aae54881edf0b38ea873745cd36476b7bcede2bb
Lin Peishan <linpeishann@gmail.com>
2024-01-20 10:10:17 +0800

    initial file structure

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+
+// import pages here!!!!
+// import dashboard from './pages/dashboard/dashboard.jsx'
+
+// react-router-dom
+import { Navigate, Routes, Route, Link, useLocation, useNavigate } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+
+    <Routes>
+          {/* <Route path="/dashboard" element={<Dashboard />} />           */}
+      </Routes>
+  </BrowserRouter>,
 )
