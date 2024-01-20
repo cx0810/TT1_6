b03210bb4dab17cc15539e9df002677cd4e753c3
Tan Jia Jia <71870881+jiajiatan@users.noreply.github.com>
2024-01-20 11:25:39 +0800

    completed table design, added search

--- frontend/src/pages/dashboard/itinerary.jsx
+++ frontend/src/pages/dashboard/itinerary.jsx
@@ -7,23 +7,50 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
+import EditIcon from "@mui/icons-material/Edit";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
 
 const itinerary = () => {
   const rows = [
     {
-      name: "test",
-      calories: "test",
-      fat: "test",
-      carbs: "test",
-      protein: "test",
+      destination: "test",
+      cost: 1,
+      notes: "test",
+    },
+    {
+      destination: "test",
+      cost: 1,
+      notes: "test",
+    },
+    {
+      destination: "test",
+      cost: 1,
+      notes: "test",
     },
   ];
 
   return (
     <div>
       <h1>Itinerary Page</h1>
-      <Button variant="contained">Create Destination</Button>
-      <TableContainer sx={{ width: 1000 }} component={Paper}>
+      <Grid
+        sx={{ mb: 1 }}
+        container
+        direction="row"
+        justifyContent="space-between"
+        justify="flex-end"
+      >
+        <Button variant="contained">Create Destination</Button>
+        <TextField
+          id="outlined-basic"
+          label="Seach destination"
+          variant="outlined"
+          align="left"
+        />
+      </Grid>
+      <TableContainer component={Paper}>
         <Table aria-label="simple table">
           <TableHead>
             <TableRow>
@@ -43,15 +70,25 @@ const itinerary = () => {
                 <TableCell component="th" scope="row">
                   {index + 1}
                 </TableCell>
-                <TableCell align="left">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="left">{row.destination}</TableCell>
+                <TableCell align="right">{row.cost}</TableCell>
+                <TableCell align="right">{row.notes}</TableCell>
+                <TableCell align="right">
+                  <div>
+                    <IconButton color="warning" aria-label="edit">
+                      <EditIcon />
+                    </IconButton>
+                    <IconButton color="error" aria-label="delete">
+                      <DeleteIcon />
+                    </IconButton>
+                  </div>
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
+      <p align="right">Total Budget: </p>
     </div>
   );
 };
