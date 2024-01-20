a4b68a0c3313d69bd7826099777d04ac735128df
Tim <timcheah1998@gmail.com>
2024-01-20 15:55:58 +0800

    integrate register

--- backend/express/server.js
+++ backend/express/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import authRouter from "./authRouter.js";
+import cors from "cors";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3030;
 
 const app = express();
+// Enable CORS for all routes and origins
+app.use(cors());
 
 app.use(express.json());
 app.use("/api/v1/user", authRouter);
