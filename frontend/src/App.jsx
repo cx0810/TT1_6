eb1bcb080e035a8acdb5afcab1da9037dc08bca7
Tan Jia Jia <71870881+jiajiatan@users.noreply.github.com>
2024-01-20 10:24:52 +0800

    initial routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/dashboard/dashboard";
+import Itinerary from "./pages/dashboard/itinerary";
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <Routes>
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="itinerary" element={<Itinerary />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
